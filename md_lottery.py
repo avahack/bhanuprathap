84b7f038235277265ad70e585fb59076d7d927ed
Bhanuprathap Reddy <60850856+avahack@users.noreply.github.com>
2024-01-29 15:26:28 -0500

    Update md_lottery.py

--- md_lottery.py
+++ md_lottery.py
@@ -1,265 +1,39 @@
-output_1 = {}
-output_2 = {}
-output_3 = {}
-output_5 = {}
-output_10 = {}
-output_20 = {}
-output_30 = {}
-output_A5 = {}
-output_A10 = {}
+def get_input(prompt, num_values):
+    return {f"{prompt}{i+1}": int(input(f"Enter the {prompt}'s value of {i+1}: ")) for i in range(num_values)}
 
-print("Enter the yesterday numbers, Please only enter the numbers.\n")
+def calculate_output(today_input, yesterday_input, multiplier):
+    return {key: (today_input[key] - yesterday_input.get(key, 0)) * multiplier for key in today_input}
 
-yesterday_input_30 = {
-    'A': int(input("Enter the yesterday's value of A: ")),
-    'B': int(input("Enter the yesterday's value of B: "))
-}
-# list_30 = list(yesterday_input_30.values())
-# print(list_30) // [i * 5 for i in my_list]
-print(yesterday_input_30)
-yesterday_input_20 = {
-    'C': int(input("Enter the yesterday's value of C: ")),
-    'D': int(input("Enter the yesterday's value of D: ")),
-    'E': int(input("Enter the yesterday's value of E: ")),
-    'F': int(input("Enter the yesterday's value of F: ")),
-    'G': int(input("Enter the yesterday's value of G: "))
-}
-print(yesterday_input_20)
-yesterday_input_10 = {
-    "1":int(input("Enter the yesterday's value of 1: ")),
-    "2":int(input("Enter the yesterday's value of 2: ")),
-    "3":int(input("Enter the yesterday's value of 3: ")),
-    "4":int(input("Enter the yesterday's value of 4: ")),
-    "5":int(input("Enter the yesterday's value of 5: ")),
-    "6":int(input("Enter the yesterday's value of 6: ")),
-    "7":int(input("Enter the yesterday's value of 7: ")),
-    "8":int(input("Enter the yesterday's value of 8: ")),
-    "9":int(input("Enter the yesterday's value of 9: ")),
-    "10":int(input("Enter the yesterday's value of 10: ")),
-    "11":int(input("Enter the yesterday's value of 11: ")),
-    "12":int(input("Enter the yesterday's value of 12: ")),
-    "13":int(input("Enter the yesterday's value of 13: ")),
-    "14":int(input("Enter the yesterday's value of 14: ")),
-    "15":int(input("Enter the yesterday's value of 15: ")),
-    "16":int(input("Enter the yesterday's value of 16: ")),
-    "17":int(input("Enter the yesterday's value of 17: ")),
-    "18":int(input("Enter the yesterday's value of 18: ")),
-}
-print(yesterday_input_10)
-yesterday_input_5 = {
-    "19":int(input("Enter the yesterday's value of 19: ")),
-    "20":int(input("Enter the yesterday's value of 20: ")),
-    "21":int(input("Enter the yesterday's value of 21: ")),
-    "22":int(input("Enter the yesterday's value of 22: ")),
-    "23":int(input("Enter the yesterday's value of 23: ")),
-    "24":int(input("Enter the yesterday's value of 24: ")),
-    "25":int(input("Enter the yesterday's value of 25: ")),
-    "26":int(input("Enter the yesterday's value of 26: ")),
-    "27":int(input("Enter the yesterday's value of 27: ")),
-    "28":int(input("Enter the yesterday's value of 28: ")),
-    "29":int(input("Enter the yesterday's value of 29: ")),
-    "30":int(input("Enter the yesterday's value of 30: ")),
-    "31":int(input("Enter the yesterday's value of 31: ")),
-    "32":int(input("Enter the yesterday's value of 32: ")),
-    "33":int(input("Enter the yesterday's value of 33: ")),
-    "34":int(input("Enter the yesterday's value of 34: ")),
-    "35":int(input("Enter the yesterday's value of 35: ")),
-    "36":int(input("Enter the yesterday's value of 36: ")),    
-}
-print(yesterday_input_5)
-yesterday_input_3 = {
-    "37":int(input("Enter the yesterday's value of 37: ")),
-    "38":int(input("Enter the yesterday's value of 38: ")),
-    "39":int(input("Enter the yesterday's value of 39: ")),
-    "40":int(input("Enter the yesterday's value of 40: ")),
-}
-print(yesterday_input_3)
-yesterday_input_2 = {
-    "41":int(input("Enter the yesterday's value of 41: ")),
-    "42":int(input("Enter the yesterday's value of 42: ")),
-    "43":int(input("Enter the yesterday's value of 43: ")),
-    "44":int(input("Enter the yesterday's value of 44: ")),
-    "45":int(input("Enter the yesterday's value of 45: ")),
-}
-print(yesterday_input_2)
-yesterday_input_1 = {
-    "46":int(input("Enter the yesterday's value of 46: ")),
-    "47":int(input("Enter the yesterday's value of 47: ")),
-    "48":int(input("Enter the yesterday's value of 48: ")),
-}
-print(yesterday_input_1)
-yesterday_input_A5 = {
-    "49":int(input("Enter the yesterday's value of 49: ")),
-    "50":int(input("Enter the yesterday's value of 50: ")),
-    "51":int(input("Enter the yesterday's value of 51: ")),
-    "52":int(input("Enter the yesterday's value of 52: ")),
-    "53":int(input("Enter the yesterday's value of 53: ")),
-    "54":int(input("Enter the yesterday's value of 54: ")),
-}
-print(yesterday_input_A5)
-yesterday_input_A10 = {
-    "55":int(input("Enter the yesterday's value of 55: "))
-}
-print(yesterday_input_A10)
-print("Please Enter today's numbers. Before entering today's numbers, verify yesterday's numbers.\n")
+def print_summary(output):
+    total = sum(output.values())
+    print(f"Today's summary is: {total}")
+    return total
 
-today_input_30 = {
-    'A': int(input("Enter the today's value of A: ")),
-    'B': int(input("Enter the today's value of B: "))
-}
-print(today_input_30)
+def main():
+    prompts = ['A', 'B', 'C', 'D', 'E', 'F', 'G', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '40', '41', '42', '43', '44', '45', '46', '47', '48', '49', '50', '51', '52', '53', '54', '55']
+    multipliers = [30, 20, 10, 5, 3, 2, 1, 5, 10]
+    num_values = [2, 5, 18, 18, 4, 5, 3, 6, 1]
 
-today_input_20 = {
-    'C': int(input("Enter the today's value of C: ")),
-    'D': int(input("Enter the today's value of D: ")),
-    'E': int(input("Enter the today's value of E: ")),
-    'F': int(input("Enter the today's value of F: ")),
-    'G': int(input("Enter the today's value of G: "))
-}
-print(today_input_20)
-today_input_10 = {
-    "1":int(input("Enter the today's value of 1: ")),
-    "2":int(input("Enter the today's value of 2: ")),
-    "3":int(input("Enter the today's value of 3: ")),
-    "4":int(input("Enter the today's value of 4: ")),
-    "5":int(input("Enter the today's value of 5: ")),
-    "6":int(input("Enter the today's value of 6: ")),
-    "7":int(input("Enter the today's value of 7: ")),
-    "8":int(input("Enter the today's value of 8: ")),
-    "9":int(input("Enter the today's value of 9: ")),
-    "10":int(input("Enter the today's value of 10: ")),
-    "11":int(input("Enter the today's value of 11: ")),
-    "12":int(input("Enter the today's value of 12: ")),
-    "13":int(input("Enter the today's value of 13: ")),
-    "14":int(input("Enter the today's value of 14: ")),
-    "15":int(input("Enter the today's value of 15: ")),
-    "16":int(input("Enter the today's value of 16: ")),
-    "17":int(input("Enter the today's value of 17: ")),
-    "18":int(input("Enter the today's value of 18: ")),
-}
-print(today_input_10)
-today_input_5 = {
-    "19":int(input("Enter the today's value of 19: ")),
-    "20":int(input("Enter the today's value of 20: ")),
-    "21":int(input("Enter the today's value of 21: ")),
-    "22":int(input("Enter the today's value of 22: ")),
-    "23":int(input("Enter the today's value of 23: ")),
-    "24":int(input("Enter the today's value of 24: ")),
-    "25":int(input("Enter the today's value of 25: ")),
-    "26":int(input("Enter the today's value of 26: ")),
-    "27":int(input("Enter the today's value of 27: ")),
-    "28":int(input("Enter the today's value of 28: ")),
-    "29":int(input("Enter the today's value of 29: ")),
-    "30":int(input("Enter the today's value of 30: ")),
-    "31":int(input("Enter the today's value of 31: ")),
-    "32":int(input("Enter the today's value of 32: ")),
-    "33":int(input("Enter the today's value of 33: ")),
-    "34":int(input("Enter the today's value of 34: ")),
-    "35":int(input("Enter the today's value of 35: ")),
-    "36":int(input("Enter the today's value of 36: ")),
-}
-print(today_input_5)
-today_input_3 = {
-    "37":int(input("Enter the today's value of 37: ")),
-    "38":int(input("Enter the today's value of 38: ")),
-    "39":int(input("Enter the today's value of 39: ")),
-    "40":int(input("Enter the today's value of 40: ")),
-}
-print(today_input_3)
-today_input_2 = {
-    "41":int(input("Enter the today's value of 41: ")),
-    "42":int(input("Enter the today's value of 42: ")),
-    "43":int(input("Enter the today's value of 43: ")),
-    "44":int(input("Enter the today's value of 44: ")),
-    "45":int(input("Enter the today's value of 45: ")),
-}
-print(today_input_2)
-today_input_1 = {
-    "46":int(input("Enter the today's value of 46: ")),
-    "47":int(input("Enter the today's value of 47: ")),
-    "48":int(input("Enter the today's value of 48: ")),
-}
-print(today_input_1)
-today_input_A5 = {
-    "49":int(input("Enter the today's value of 49: ")),
-    "50":int(input("Enter the today's value of 50: ")),
-    "51":int(input("Enter the today's value of 51: ")),
-    "52":int(input("Enter the today's value of 52: ")),
-    "53":int(input("Enter the today's value of 53: ")),
-    "54":int(input("Enter the today's value of 54: ")),
-}
-print(today_input_A5)
-today_input_A10 = {
-    "55":int(input("Enter the today's value of 55: "))
-}
-print(today_input_A10)
+    total_sum = 0
+    for prompt, multiplier, num in zip(prompts, multipliers, num_values):
+        yesterday_input = get_input(f"yesterday's {prompt}", num)
+        today_input = get_input(f"today's {prompt}", num)
+        output = calculate_output(today_input, yesterday_input, multiplier)
+        total_sum += print_summary(output)
 
-for key in today_input_30.keys():
-    output_30[key] = today_input_30[key] - yesterday_input_30.get(key, 0)
-list_30 = list(output_30.values())
+    cash = float(input("Enter the amount of cash: "))
+    summary = total_sum - cash
 
-for key in today_input_20.keys():
-    output_20[key] = today_input_20[key] - yesterday_input_20.get(key, 0)
-list_20 = list(output_20.values())
+    if summary == 0:
+        print("Today you guys did great job! Today summary is OK.")
+    elif summary > 0:
+        print(f"Today total is plus {summary}")
+    elif summary < 0:
+        print(f"Today total is minus {summary}")
+    else:
+        print("You did something wrong! Please try again.\n")
 
-for key in today_input_10.keys():
-    output_10[key] = today_input_10[key] - yesterday_input_10.get(key, 0)
-list_10 = list(output_10.values())
+    print("Hope this application made your work a bit easier. Developed by BHANU!")
 
-for key in today_input_5.keys():
-    output_5[key] = today_input_5[key] - yesterday_input_5.get(key, 0)
-list_5 = list(output_5.values())
-
-for key in today_input_3.keys():
-    output_3[key] = today_input_3[key] - yesterday_input_3.get(key, 0)
-list_3 = list(output_3.values())
-
-for key in today_input_2.keys():
-    output_2[key] = today_input_2[key] - yesterday_input_2.get(key, 0)
-list_2 = list(output_2.values())
-
-for key in today_input_1.keys():
-    output_1[key] = today_input_1[key] - yesterday_input_1.get(key, 0)
-list_1 = list(output_1.values())
-
-for key in today_input_A5.keys():
-    output_A5[key] = today_input_A5[key] - yesterday_input_A5.get(key, 0)
-list_A5 = list(output_A5.values())
-
-for key in today_input_A10.keys():
-    output_A10[key] = today_input_A10[key] - yesterday_input_A10.get(key, 0)
-list_A10 = list(output_A10.values())
-
-price_30 = [i * 30 for i in list_30]
-price_20 = [i * 20 for i in list_20]
-price_10 = [i * 10 for i in list_10]
-price_5 = [i * 5 for i in list_5]
-price_3 = [i * 3 for i in list_3]
-price_2 = [i * 2 for i in list_2]
-price_1 = [i * 1 for i in list_1]
-price_A5 = [i * 5 for i in list_A5]
-price_A10 = [i * 10 for i in list_A10]
-toady_summery_of_numbers = price_30 + price_20 + price_10 + price_5 + price_3 + price_2 + price_1 + price_A5 + price_A10
-today_sum = float(sum(toady_summery_of_numbers))
-print(today_sum)
-
-lott_1 = int(float(input("Enter the value of lottery machine 1: ")))
-lott_2 = int(float(input("Enter the value of lottery machine 2: ")))
-pouch = int(float(input("Enter the amount of money that you took from the pouch: ")))
-total_summery = today_sum + lott_1 + lott_2 + pouch
-print("Today's summery is: " + str(total_summery))
-
-cash = float(input("Enter the amount of cash: "))
-SUMMERY = total_summery - cash
-
-if SUMMERY == 0:
-    print("Today you guys did great job! Today summery is OK.")
-elif SUMMERY > 0:
-    print("Today total is plus " + str(SUMMERY))
-elif SUMMERY < 0:
-    print("Today total is minus " + str(SUMMERY))
-else:
-    print("You did something wrong! Please try again.\n")
-
-print("Hope this application made your work lil bit easier. Developed by BHANU!")
-
+if __name__ == "__main__":
+    main()
